a4d42bbffcf7b57eaae147ff09856c4897d4688d
GiadaPa <palmagiada98@gmail.com>
2022-08-31 17:05:56 +0200

    tried some functions

--- PlanningAndMultimTrans.py
+++ PlanningAndMultimTrans.py
@@ -5,16 +5,19 @@
 """
 
 ### Libraries
+import re
 import gurobipy as gp
 from gurobipy import *
+from sklearn.metrics import homogeneity_completeness_v_measure
 
 
 ### Sets and parameters
 
+# Dictionary containing multiple dictionaries of information
+input_data = {}
 
 # Open text file for reading data.
 txtFile = open('Demo Instances/instance_demo1_N10.txt','r')
-
 
 # Function definition to transform an element to hours by converting it to integer (eventually back to string)
 def to_hours(element):
@@ -23,37 +26,41 @@ def to_hours(element):
     return h
 
 
-
+"""
+#-------------------------------------------------------- HORIZON -----------------------------------------------
 # Dictionary definition of the horizon
 horizon_dict = {}
 
 # Function definition to obtain the horizon start and end time
-def horizon():
+def get_horizon():
     with txtFile as f:
         line = f.readline()
         horizon_dict.update({'start': to_hours(line.split()[1]), 'end': to_hours(line.split()[2])})
-        print(horizon_dict)
+    return horizon_dict
+#get_horizon() # Check if the horizon is correct
 
-horizon() # Check if the horizon is correct
-
-
-
-
-
-people = []     #Declare an empty list for the people.
-
+input_data['horizon'] = get_horizon()
+#print(input_data)
 """
-def horizon():
-    txtFile.seek(0)              
-    with txtFile as f: 
-        for line1 in f:                # For each line, stored as myline,
-            people.append(line1)           # add its contents to mylines.
-    return people[0]                          # Print the list.
+#----------------------------------------------------------------------------------------------------------------
 
-print(horizon())
-"""
+answer = {}
+with txtFile as document:
+    for line in document:
+        if not re.match("NODE_ID NODE_ID MODE DISTANCE TIME FITNESS",line):
+            if line.strip():  # non-empty line?
+                key, value = line.split(None, 1)  # None means 'all whitespace', the default
+                answer[key] = value.split()
+        else: 
+            break
+print(answer)
 
 
+def get_horizon():
+    answer.keys()
+
+
+print(get_horizon())
 ### Initialization of the problem
 
 ### Variables
