642fe3cc29c2880589e8a6f50b37b8c421ddc81d
Fabio Greenwood <96693924+FabioGreenwood@users.noreply.github.com>
2022-10-14 21:43:26 +0200

    additional of task timing second equation and corrections to documentation in task timing section

--- run_scenario.py
+++ run_scenario.py
@@ -108,7 +108,7 @@ def subtour(vals):
 
 def run_scenario(input_objects, input_links, input_global):
 
-
+    disable_costly_constraints = True
     """Begin of model creation """
     md = gp.Model()
 
@@ -143,7 +143,63 @@ def run_scenario(input_objects, input_links, input_global):
 
 
     """Constants"""
+    
+    #cost of a task
+
+    #task time window
+    const_a_t = dict()
+    const_b_t = dict()
+    
+    for task in input_objects["TASKS"].values():
+        const_a_t[task["TASK_ID"]] = task["START_TIME"]
+        const_b_t[task["TASK_ID"]] = task["END_TIME"]
+    md.update()
+    
+    
+    
+
+    #task duration
+    const_s_t = dict() #
+    for task in input_objects["TASKS"].values():
+        const_s_t[task["TASK_ID"]] = task["SERVICE_TIME"]
+    md.update()
+    
+
+    #additional time required for special task* if not done within allotted time window
+    """Action: I will have to put a methanium to enforce what is in the * set"""
+    const_st_istar = dict() #
+    for task in input_objects["TASKS"].values():
+        const_st_istar[task["TASK_ID"]] = task["EXTRA_SERVICE_TIME"]
+    md.update()
+
+    #latitude and longitude of a place
+
+    #number of bikes available and free spots at a bike station
+
+    #latitude and longitude of a bike station
+
+    #cost of bikes in Verona per minute (see assumptions)
+
+    #latitude and longitude of a bus stop
+
+    #cost of chosing bus as transportation mode
+
+    #set of bus departure times (node i,line l,departure time d)
+
+    #time window a person has to board a bus (see assumptions)
+
+    #Maximum number of people allowed on a bus (see assumptions)
+
     #the time required to travel down arc ij via mode m
+
+    #Penalty for a task t not performed
+
+    #Max number of times a person n can change transportation mode
+
+    #Boolean whether node i requests service from person n
+
+    #Boolean whether node i is person n's home
+
     #Set of all starting points of person n
     const_h_in = dict()
     for person_id in index_person_ids:
@@ -153,8 +209,6 @@ def run_scenario(input_objects, input_links, input_global):
         node_id = input_objects["PEOPLE"][person_id]["HOME_ID"]
         const_h_in[node_id, person_id] = 1
         
-        
-
     #travelling time associated to each arc
     const_t_ijm = dict()
     for i_id, j_id, mode_id in input_links["NODE_TRAVEL_INFO"].keys():
@@ -164,6 +218,7 @@ def run_scenario(input_objects, input_links, input_global):
     md.update()
     
     #this is the const_t_ijm extrended by an extra dimention (n) for use in a later constraint
+    """I will want to delete this constant"""
     const_t_ijmn = dict()
     for i_id, j_id, mode_id in input_links["NODE_TRAVEL_INFO"].keys():
         for person_id in index_person_ids:
@@ -172,26 +227,25 @@ def run_scenario(input_objects, input_links, input_global):
     del value
     md.update()
     
-    #time required to complete task
-    const_s_t = dict() #
+    #whether a task is special (can be delayed)
+    const_special_t = dict() #
     for task in input_objects["TASKS"].values():
-        const_s_t[task["TASK_ID"]] = task["SERVICE_TIME"]
-    md.update()
+        const_special_t[task["TASK_ID"]] = task["IS_SPECIAL"]
+    md.update() 
     
-    #additional time required for special task* if not done within allotted time window
-    const_st_istar = dict() #
-    for task in input_objects["TASKS"].values():
-        const_st_istar[task["TASK_ID"]] = task["EXTRA_SERVICE_TIME"]
-    md.update()
     
     
     """M (large) Constants"""
-    M_task_timing = input_global["END"] * 100
+    M_time = input_global["END"] * 100
     
 
-    """Independent Variables"""    
+    """Independent Variables"""
+    
+    
+    #Amount of money of person n
+    
+    
     #whether person n travels down arc ij on tranportation mode m (Bool)
-    #Here every combination of two lists need to be produced, to ensure we have a sparse various
     list_to_combine = [list(input_links["NODE_TRAVEL_INFO"].keys()), list(index_person_ids)]
     links_person_combination_list_pre = list(itertools.product(*list_to_combine))
     links_person_combination_list = []
@@ -201,6 +255,7 @@ def run_scenario(input_objects, input_links, input_global):
       
     x_vars = md.addVars(link_person_combined_dict.keys(), vtype=GRB.BINARY, name='x_var') 
     
+    
     #whether person n completes task t at node i
     y_vars = gp.tupledict()
     y_var_string = "y_var_i{}_t{}_n{}"
@@ -209,10 +264,42 @@ def run_scenario(input_objects, input_links, input_global):
         person_id = input_objects["TASKS"][tasks_id]["PERSON_ID"]
         y_vars[location_id, tasks_id, person_id] = md.addVar(vtype=GRB.BINARY, name=y_var_string.format(location_id, tasks_id, person_id))
     
+    #Boolean,True if task is not done outside allotted time
+    #Constrained to zero for tasks where this isn^' t an option
+    #these tasks suffer extended times
+    tstar_vars = gp.tupledict()
+    tstar_var_string = "tstar_var_t{}"
+    for tasks_id in input_objects["TASKS"].keys():
+        tstar_vars[tasks_id] = md.addVar(vtype=GRB.BINARY, name=tstar_var_string.format(tasks_id))
+    
+    
+    #quantity of bikes available at node i at the end of period i
+    
+    
+    #quantity of bike spaces available at node i at the end of period i
+    
+    
+    #whether fare for bike is incurred at node i
+    
+    
+    #whether person n leaves node i via line l at departure number d
+    
+    
+    #whether fare for bus is incurred for node i,line l,departure time d
+    
+    
+    #Boolean whether node i is serviced by person n
+    
+    
+    #health or loss gain according to transportation mode chosen
+    
+    
+    
     
     
     """Semi-Dependant Variables"""
     #These are the variables that are technically dependant variables but are modelled as constrained independent variables
+    print("Semi-Dependant Variables")
     w_vars = gp.tupledict()
     w_var_string = "w_var_i{}_n{}"
     for node_id in index_nodes_ids:
@@ -239,6 +326,7 @@ def run_scenario(input_objects, input_links, input_global):
     #Flow is directional, multi-medium, multi-flow (person) 
     #[the entries/exits from a node j, needs to be larger than one if there is a task at the node or if it is the home node of the person (h) (Boolean values)]
     # "BCoFO" -> (out)
+    print("BCoFO -> (out)")
     constr_BCoFO_string = "const_BCoFO_np[{},{}]"    
     for node_id in index_nodes_ids:
         for person_id in index_person_ids:
@@ -276,42 +364,71 @@ def run_scenario(input_objects, input_links, input_global):
     #Task Timing
     #This controls the time (w) which the task at j starts
     #TT1_ijntm
-    for i in index_nodes_ids:
-        for j in index_nodes_ids:
-            for n in index_person_ids:
-                for t in index_task_ids:
-                    for m in index_modes_of_transport:
-                        if i != j and return_if_valid_reference(x_vars, [i, j, m, n], False, True):
-                            expr_a_temp = w_vars[j, n] - w_vars[i, n] 
-                            expr_b_temp = - x_vars[i, j, m, n] * const_t_ijm[i,j,m]
-                            #These expresions only count if there is a task for the person/node/task combination
-                            if return_if_valid_reference(y_vars, [i, t, n], False, True):
-                                expr_c_temp = - (const_s_t[t] * y_vars[i, t, n]) - (const_st_istar[t] * ts_istar_vars[t])
-                            else:
-                                expr_c_temp = 0 
-                            #expr_b_temp = 0
-                            #for m in index_modes_of_transport:
-                            #    if return_if_valid_reference(x_vars, [i, j, m, n], False, True):
-                            #        expr_b_temp += x_vars[i, j, m, n] * const_t_ijm[i,j,m] 
-                            expr_d_temp = - M_task_timing * (1 - x_vars[i,j,m,n])
-                            constr_name_string =  "TT1_i{}j{}n{}t{}m{}"
-                            md.addConstr((expr_a_temp + expr_b_temp + expr_c_temp - expr_d_temp >= 0), name = constr_name_string.format(i,j,n,t,m,))
-                            
-                            
-                            """Do not delete, this was the previous requirement to make the constraint, but is still a good example to kepp"""
-                            """expression_temp = 0
-                            for j in index_nodes_ids:
-                                for m in index_modes_of_transport:
-                                    if return_if_valid_reference(x_vars, [i, j, m, n], False, True):
-                                        expression_temp += x_vars[i, j, m, n] * const_t_ijm[i,j,m] 
-                            md.addConstr((expression_temp>=1), name = "Test2")"""
+    print("TT1_ijntm")
+    
+    
+    if disable_costly_constraints == False:
+        for i in index_nodes_ids:
+            print(i)
+            for j in index_nodes_ids:
+                for n in index_person_ids:
+                    for t in index_task_ids:
+                        for m in index_modes_of_transport:
+                            if i != j and return_if_valid_reference(x_vars, [i, j, m, n], False, True):
+                                expr_a_temp = w_vars[j, n] - w_vars[i, n] 
+                                expr_b_temp = - x_vars[i, j, m, n] * const_t_ijm[i,j,m]
+                                #These expresions only count if there is a task for the person/node/task combination
+                                if return_if_valid_reference(y_vars, [i, t, n], False, True):
+                                    expr_c_temp = - (const_s_t[t] * y_vars[i, t, n]) - (const_st_istar[t] * ts_istar_vars[t])
+                                else:
+                                    expr_c_temp = 0 
+                                expr_d_temp = - M_time * (1 - x_vars[i,j,m,n])
+                                constr_name_string =  "TT1_i{}j{}n{}t{}m{}"
+                                
+                                md.addConstr((expr_a_temp + expr_b_temp + expr_c_temp - expr_d_temp >= 0), name = constr_name_string.format(i,j,n,t,m,))
+                                
+                                """Do not delete, this was the previous requirement to make the constraint, but is still a good example to kepp"""
+                                """expression_temp = 0
+                                for j in index_nodes_ids:
+                                    for m in index_modes_of_transport:
+                                        if return_if_valid_reference(x_vars, [i, j, m, n], False, True):
+                                            expression_temp += x_vars[i, j, m, n] * const_t_ijm[i,j,m] 
+                                md.addConstr((expression_temp>=1), name = "Test2")"""
 
-                            #md.addConstr((sum(x_vars[i, j, m, n] * const_t_ijm[i,j,m] for j in index_nodes_ids for m in index_modes_of_transport if return_if_valid_reference(x_vars, [i, j, m, n], False, True))>1), name = "Test2")
-                            
-                            md.update()
-                            md.write(explicit_output_folder_location + "model_export.lp")
-    del expr_a_temp, expr_b_temp, expr_c_temp, expr_d_temp
+                                #md.addConstr((sum(x_vars[i, j, m, n] * const_t_ijm[i,j,m] for j in index_nodes_ids for m in index_modes_of_transport if return_if_valid_reference(x_vars, [i, j, m, n], False, True))>1), name = "Test2")
+        del expr_a_temp, expr_b_temp, expr_c_temp, expr_d_temp
+    
+    md.update()
+    md.write(explicit_output_folder_location + "model_export.lp")
+    
+    #Task Timing (cont)
+    #This controls that tasks happen within their designated time windows (only applies if the tasks happens 
+    #and the task extension penalty for falling out of the allotted time isn’t applied)
+    #TT2before_int & TT2after_int
+    
+    TT2after_name_string =  "TT2after_i{}n{}t{}"
+    TT2before_name_string =  "TT2before_i{}n{}t{}"
+    for i in index_nodes_ids:
+        for n in index_person_ids:
+            for t in index_task_ids:
+                if return_if_valid_reference(y_vars, [i, t, n], False, True):
+                    md.addConstr((const_a_t[t] - M_time * tstar_vars[t] - M_time * (1 - y_vars[i, t, n]) <= w_vars[i, n]), name = TT2after_name_string.format(i,n,t))
+                    md.addConstr((const_b_t[t] + M_time * tstar_vars[t] + M_time * (1 - y_vars[i, t, n]) >= w_vars[i, n]), name = TT2before_name_string.format(i,n,t))
+                    
+                    
+                
+    md.update()
+    md.write(explicit_output_folder_location + "model_export.lp")
+                    
+                
         
+    
+    
+    
+    
+    
+    
+    
     
     print("Stop")
     
