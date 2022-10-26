404c4e40d0f97613014ec1ff0302c2e59d3b10a2
Fabio Greenwood <96693924+FabioGreenwood@users.noreply.github.com>
2022-10-26 16:50:38 +0200

    general psuh, before presentation

--- run_scenario.py
+++ run_scenario.py
@@ -22,7 +22,8 @@ from os.path import isfile, join
 from datetime import datetime
 import copy
 import pathlib
-
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 
 from import_function import import_inputs
@@ -845,8 +846,49 @@ def export_results(model, input_global, input_objects, input_links, index_person
             tour_times = tour_times + [w_vars[stop,n_].X]
         tour_times = tour_times + ["End of day"]
         output_people_route_times[n_] = tour_times
+    print("Hello")
     
+def visualise_results_and_export():
+    places_x_coords = [place["PLACE_LON"] for place in input_objects["PLACES"].values()]
+    places_y_coords = [place["PLACE_LAT"] for place in input_objects["PLACES"].values()]
+    person_route    = [1,2,3]
+    person_modes    = ["WALKING", "BUS"]
+
+    visited_places_x_coords = []
+    visited_places_y_coords = []
+    for i in person_route:
+        visited_places_x_coords = visited_places_x_coords + [places_x_coords[i]]
+        visited_places_y_coords = visited_places_y_coords + [places_y_coords[i]]
+
+
+    for i in range(0, len(visited_places_x_coords)-1):
+            match person_modes[i]:
+                case "WALKING":
+                    color = "green"
+                case "CYCLING":
+                    color = "orange"
+                case "BUS":
+                    color = "red"
+            
+            plt.plot([visited_places_x_coords[i], visited_places_x_coords[i+1]], [visited_places_y_coords[i], visited_places_y_coords[i+1]], 'ro-', color = color)
+            
+
+
+    plt.xlabel('Long')
+    plt.ylabel('Lat')
+    plt.title('Route Map')
+    plt.scatter(places_x_coords, places_y_coords, s=20)
+    plt.scatter(visited_places_x_coords, visited_places_y_coords, s=20)
+    for i in range(0, len(places_x_coords)):
+        plt.annotate(i, (places_x_coords[i], places_y_coords[i]))
+    #ax1.scatter(range(study_range), pred_input[:study_range], s=20)
+    plt.legend(('Places',"visited"))
+
     
+    plt.show()
+
+
+
     
 
 """Temporary section"""
