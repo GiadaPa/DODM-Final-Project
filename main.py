5f7005f747c4a1ac56e9c1fd7a1e29ae4120d04d
Fabio Greenwood <96693924+FabioGreenwood@users.noreply.github.com>
2022-10-30 00:47:01 +0200

    project complete, will upload output for run later

--- main.py
+++ main.py
@@ -21,23 +21,26 @@ from import_function import import_inputs
 from run_scenario import run_scenario
 from os import listdir
 from os.path import isfile, join
+from datetime import datetime
+import pathlib
 exec(open("import_function.py").read())
 exec(open("run_scenario.py").read())
 
 
 
-explicit_input_folder_location = "C:/Users/fabio/OneDrive/Documents/Studies/Discrete_Optimisation/DODM-Final-Project/Demo Instances/inputs_test/"
-explicit_output_folder_location = "C:/Users/fabio/OneDrive/Documents/Studies/Discrete_Optimisation/DODM-Final-Project/Demo Instances/outputs_test/"
+explicit_input_folder_location  = str(pathlib.Path(__file__).parent.resolve()) + "\\Demo Instances\\inputs_test\\"
+explicit_output_folder_location = str(pathlib.Path(__file__).parent.resolve()) + "\\Demo Instances\\outputs_test\\"
 input_file_names = [f for f in listdir(explicit_input_folder_location) if isfile(join(explicit_input_folder_location, f))]
 
 #this will loop all the inputs we have put in the folder
+count = 0
 for scenario_input_file in input_file_names:
     input_objects, input_links, input_global = import_inputs(explicit_input_folder_location + scenario_input_file)
-    run_scenario(input_objects, input_links, input_global)
-    #results_string = run_scenario(input_objects, input_links, input_global, solver = solver)
-    #create csv output -> explicit_output_folder_location
-    
-    
-
-
-print("Hello")
+    count += 1
+    print(scenario_input_file + " start at: " + str(datetime.now()) + str(count) + " / " + str(len(input_file_names)))
+    run_scenario(input_objects, input_links, input_global,
+                 scenario_name = scenario_input_file, 
+                 rum_lim_minutes = 1, 
+                 disable_costly_constraints = False, 
+                 force_1_to_catch_a_bus = False,
+                 show_fig = False)
